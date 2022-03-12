0f78e58416eaa1f9699ff018d1ede5825f3fdb14
jayshreeeee <jayshree.raghavan@chicagobooth.edu>
2022-03-12 14:24:59 +0000

    added associations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,9 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+
+  has_many :characters
+  has_many :filmography, :through => :characters, :source => :movie 
+
+  
 end
